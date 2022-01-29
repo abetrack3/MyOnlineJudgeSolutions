1b25404290c05b946bdc7bcc797b54bbfaabc632
Abrar <abetrack3@yahoo.com>
2022-01-29 20:29:59 +0600

    All Reset
    Deleting all old files before reorganizing repositories. All the deleted files will be added later.

--- URI/Problem1016.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Problem1016
-{
-  public static void main (String[]args)
-  {
-    Scanner puttingvaluesinside=new Scanner (System.in);
-    System.out.println(2*puttingvaluesinside.nextInt()+" minutos");
-  }
-}
--- URI/Problem1095.java
+++
@@ -1,8 +0,0 @@
-public class Problem1095
-{
-  public static void main (String[]args)
-  {
-    for(int a=1,b=60;b>=0;a+=3,b-=5)
-      System.out.println("I="+a+" J="+b);
-  }
-}
