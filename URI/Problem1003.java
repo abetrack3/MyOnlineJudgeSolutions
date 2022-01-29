eeacaeeb359cb89870c1626643aca9a22f6a808c
Abrar <abetrack3@yahoo.com>
2022-01-29 22:16:15 +0600

    URI1003: Simple Sum
    URI1003: Simple Sum
    Link to the problem: https://www.beecrowd.com.br/judge/en/problems/view/1003

---
+++ URI/Problem1003.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Problem1003
+{
+  public static void main (String[]args)
+  {
+    Scanner puttingvalueinside = new Scanner (System.in);
+    int A = puttingvalueinside.nextInt();
+    int B = puttingvalueinside.nextInt();
+    int SOMA = A + B;
+    System.out.print("SOMA = " + SOMA);
+    System.out.println();
+  }
+}
