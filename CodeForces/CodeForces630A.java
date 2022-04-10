b503e98f2b163775a150d9309970380687d7f5e5
Abrar Shahriar <abetrack3@yahoo.com>
2022-04-10 23:46:16 +0600

    CodeForces630A: A. Again Twenty Five!
    CodeForces630A: A. Again Twenty Five!
    Link to the problem: https://codeforces.com/contest/630/problem/A

---
+++ CodeForces/CodeForces630A.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String[]args)
+    {
+        new Scanner(System.in).nextLong();
+        System.out.println(25);
+    }
+}
