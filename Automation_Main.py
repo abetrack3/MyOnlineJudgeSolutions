55db48573c30dd7dd620d9c4fe08ec46f43b944f
Abrar Shahriar <abetrack3@yahoo.com>
2022-05-05 21:11:38 +0600

    Added support for automating git commits of HackerRank problems

--- Automation_Main.py
+++ Automation_Main.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 from Automation_CodeForces import code_forces
 from SeleniumWebDriver import WebDriverFactory
@@ -18,7 +19,42 @@ DELAY_TIMEOUT = 10
 
 ################## Judge Wise Automation Functions ##################
 
-def leet_code(description, driver):
+def hacker_rank(description, driver : WebDriver):
+    # Getting the Problem Title
+    vjudge_link = f'https://vjudge.net/problem/{description[0]}-{description[1]}'
+    driver.get(vjudge_link)
+    try:
+        elem = WebDriverWait(driver, DELAY_TIMEOUT).until(
+            EC.presence_of_element_located((By.XPATH, '//*/div[@id="prob-title"]/h2'))
+        )
+        question_title = elem.text
+    except TimeoutException:
+        print("Loading took too much time!")
+        return
+    print(question_title)
+
+    # Getting the original problem link
+    driver.get(f'{vjudge_link}/origin')
+    hackerrank_link = driver.current_url
+    print(hackerrank_link)
+
+    # git add
+    file_name = f'{description[0]}-{description[1]}.{description[2]}'
+    command_git_add = f'git add HackerRank/{file_name}'
+    print(command_git_add)
+    os.popen(command_git_add)
+    time.sleep(1)
+
+    # git commit
+    message = f'HackerRank {question_title}'
+    message_description = f'{message}\nLink to the problem: {hackerrank_link}'
+    command_git_commit = f'git commit -m "{message}" -m "{message_description}"'
+    print(command_git_commit)
+    os.popen(command_git_commit)
+    time.sleep(1)
+
+
+def leet_code(description, driver : WebDriver):
     file_name = f'{description[0]}-{description[1]}.{description[2]}'
     print(file_name)
     link = f'https://leetcode.com/problems/{description[1]}/'
@@ -54,9 +90,10 @@ def leet_code(description, driver):
 def __main__():
     judge_map = {
         'LeetCode': leet_code,
-        'CodeForces': code_forces
+        'CodeForces': code_forces,
+        'HackerRank': hacker_rank
     }
-    judge_list = ['LeetCode', 'CodeForces']
+    judge_list = judge_map.keys()
 
     command_git_status = 'git status --untracked-files'
 
