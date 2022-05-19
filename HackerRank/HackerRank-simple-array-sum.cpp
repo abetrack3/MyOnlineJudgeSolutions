b84509b5b6db80bb6b9ccd1595e5bc9d0b31f195
Abrar Shahriar <abetrack3@yahoo.com>
2022-05-20 04:42:26 +0600

    HackerRank Simple Array Sum
    HackerRank Simple Array Sum
    Link to the problem: https://www.hackerrank.com/contests/hackerolympicscodechallenge/challenges/simple-array-sum

---
+++ HackerRank/HackerRank-simple-array-sum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int temp;
+        cin>>temp;
+        sum+=temp;
+    }
+    cout<<sum<<endl;
+
+    return 0;
+}
