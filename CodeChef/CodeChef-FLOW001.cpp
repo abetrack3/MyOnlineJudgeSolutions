ba7454b6fe8d4796204ba2cf59ffa2529adcb48f
Abrar Shahriar <abetrack3@yahoo.com>
2022-06-03 07:29:27 +0600

    CodeChef-FLOW001: Add Two Numbers
    CodeChef-FLOW001: Add Two Numbers
    Link to the problem: https://codechef.com/problems/FLOW001

---
+++ CodeChef/CodeChef-FLOW001.cpp
@@ -0,0 +1,21 @@
+// Problem B
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+//    int c=1;
+    cin>>n;
+    while(n--)
+    {
+        int a;
+        int b;
+        cin>>a;
+        cin>>b;
+        cout<<a+b<<endl;
+//        c++;
+    }
+
+    return 0;
+}
