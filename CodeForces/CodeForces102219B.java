6e44cb5a3beda327c29ce1653ea30663e0edaac0
Abrar Shahriar <abetrack3@yahoo.com>
2022-06-20 08:19:39 +0600

    CodeForces102219B: B. SpongeBob SquarePants
    CodeForces102219B: B. SpongeBob SquarePants
    2019 ICPC Malaysia National
    Link to the problem: https://codeforces.com/gym/102219/problem/B

---
+++ CodeForces/CodeForces102219B.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			System.out.println((sc.nextInt()==sc.nextInt())?"YES":"NO");
+		}
+	}
+}
