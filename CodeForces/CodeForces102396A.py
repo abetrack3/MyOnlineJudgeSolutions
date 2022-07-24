9f7a7da5a50ed4b85af25aa7a1f5f80c64b182b9
Abrar Shahriar <abetrack3@yahoo.com>
2022-07-25 00:18:57 +0600

    CodeForces102396A: A. King's Inspection
    CodeForces102396A: A. King's Inspection
    2019-2020 Saint-Petersburg Open High School Programming Contest (SpbKOSHP 19)
    Link to the problem: https://codeforces.com/gym/102396/problem/A

---
+++ CodeForces/CodeForces102396A.py
@@ -0,0 +1,10 @@
+# your code goes here
+arr = []
+arr.append(int(input()))
+arr.append(int(input()))
+arr.append(int(input()))
+arr.sort()
+# for x in arr:
+# 	print(x)
+ 
+print(2 * (arr[1] - arr[0]) + (arr[2] - arr[1]))
