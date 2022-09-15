6e590c9cb1b852d4d44194b43a1363a3ace6f0be
abetrack3 <abetrack3@yahoo.com>
2022-09-15 07:24:32 +0600

    LightOJ1189: Sum of Factorials
    LightOJ1189: Sum of Factorials
    Link to the problem: https://lightoj.com/problem/sum-of-factorials

---
+++ LightOJ/LightOJ1189.java
@@ -0,0 +1,193 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author abetrack3
+ */
+
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Main
+{
+    
+    public static long arr[] = new long[21];
+    
+    public static void main(String[] args) throws Exception
+    {
+        run();
+    }
+    
+    public static void run() throws Exception
+    {
+        build();
+//        System.out.println(java.util.Arrays.toString(arr));
+        Reader sc = new Reader();
+        OutputStream out = new BufferedOutputStream(System.out);
+        int q = sc.nextInt();
+        for(int i = 1; i <= q; i++)
+        {
+            long t = sc.nextLong();
+//            System.out.println("Case "+i+": "+solve(t));
+            out.write(("Case "+i+": "+solve(t)+"\n").getBytes());
+        }
+        out.flush();
+    }
+    
+    public static String solve(long t)
+    {
+        String s = "";
+        long sum = 0;
+        for(int i = arr.length - 1; i >= 0; i--)
+        {
+            if( t >= sum + arr[i])
+            {
+                sum += arr[i];
+                if(s.equals(""))
+                    s = i+"!";
+                else
+                    s = i+"!+"+s;
+            }
+        }
+        if (sum != t)
+            return "impossible";
+        else
+            return s;
+    }
+    
+    public static void build()
+    {
+        arr[0] = arr[1] = 1;
+        for(int i = 2; i < arr.length; i++)
+            arr[i] = i * arr[i - 1];
+    }
+    
+    static class Reader 
+    { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer; 
+        private int bufferPointer, bytesRead; 
+  
+        public Reader() 
+        { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public Reader(String file_name) throws IOException 
+        { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public String readLine() throws IOException 
+        { 
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c; 
+            while ((c = read()) != -1) 
+            { 
+                if (c == '\n') 
+                    break; 
+                buf[cnt++] = (byte) c; 
+            } 
+            return new String(buf, 0, cnt); 
+        } 
+  
+        public int nextInt() throws IOException 
+        { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do
+            { 
+                ret = ret * 10 + c - '0'; 
+            }  while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public long nextLong() throws IOException 
+        { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public double nextDouble() throws IOException 
+        { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+  
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (c == '.') 
+            { 
+                while ((c = read()) >= '0' && c <= '9') 
+                { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            } 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        private void fillBuffer() throws IOException 
+        { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+            if (bytesRead == -1) 
+                buffer[0] = -1; 
+        } 
+  
+        private byte read() throws IOException 
+        { 
+            if (bufferPointer == bytesRead) 
+                fillBuffer(); 
+            return buffer[bufferPointer++]; 
+        } 
+  
+        public void close() throws IOException 
+        { 
+            if (din == null) 
+                return; 
+            din.close(); 
+        } 
+    }
+    
+}
