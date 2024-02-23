e8c4140b88e7773756adccc4a89e898c6d98f2a0
Abrar Shahriar <abetrack3@yahoo.com>
2024-02-23 23:34:23 +0600

    UVA-10055: Hashmat the Brave Warrior
    UVA-10055: Hashmat the Brave Warrior
    Link to the problem: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=996

---
+++ UVA/UVA-10055.cpp
@@ -0,0 +1,15 @@
+// Problem C
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a = 0;
+    long long int b = 0;
+    while(cin>>a>>b)
+    {
+//        cin>>b;
+        cout<<abs(b-a)<<endl;
+    }
+
+    return 0;
+}
