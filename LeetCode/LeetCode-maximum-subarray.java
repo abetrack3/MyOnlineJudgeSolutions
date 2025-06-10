f83c47a7aebbd4ee7dfcaa3e359974ef2b7e2967
BRACITS\abrar.shahriar <abrar.shahriar@bracits.com>
2025-06-10 15:24:56 +0600

    LeetCode 53. Maximum Subarray
    LeetCode 53. Maximum Subarray

---
+++ LeetCode/LeetCode-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    // Leetcode 53: Maximum Subarray
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
